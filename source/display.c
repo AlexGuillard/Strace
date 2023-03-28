81ec4fde1f3da9d47f4b8203e8cb2c235ba97dda
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-03-28 10:31:57 +0200

    [edit] - display.c - add condition print arg

--- source/display.c
+++ source/display.c
@@ -9,26 +9,28 @@
 
 int get_number_arg(syscall_t system_call, int i)
 {
-    if (i == 1)
+    if (i == 0)
         return system_call.rdi;
-    if (i == 2)
+    if (i == 1)
         return system_call.rsi;
-    if (i == 3)
+    if (i == 2)
         return system_call.rdx;
-    if (i == 4)
+    if (i == 3)
         return system_call.rcx;
-    if (i == 5)
+    if (i == 4)
         return system_call.r8;
-    if (i == 6)
+    if (i == 5)
         return system_call.r9;
     return 0;
 }
 
 void display_info(syscall_t system_call)
 {
+    if (strcmp(system_call.name, "?") == 0)
+        return;
     printf("%s(", system_call.name);
     for (int i = 0; i < system_call.nb_arg; i++) {
-        if (get_number_arg(system_call, i) != 0)
+        if (system_call.nb_arg > 1)
             printf("%#x", get_number_arg(system_call, i));
         if (i + 1 < system_call.nb_arg)
             printf(", ");
