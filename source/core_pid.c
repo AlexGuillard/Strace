4084ad0c0702880ae621b3f22c38b62838927df6
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-03-28 10:06:51 +0200

    [rn] - core.c -> core_pid.c

--- source/core_pid.c
+++ source/core_pid.c
@@ -18,35 +18,6 @@ int detection_pid_pos(char **av)
     return pos_pid;
 }
 
-int get_number_arg(syscall_t system_call, int i)
-{
-    if (i == 1)
-        return system_call.rdi;
-    if (i == 2)
-        return system_call.rsi;
-    if (i == 3)
-        return system_call.rdx;
-    if (i == 4)
-        return system_call.rcx;
-    if (i == 5)
-        return system_call.r8;
-    if (i == 6)
-        return system_call.r9;
-    return 0;
-}
-
-void display_info(syscall_t system_call)
-{
-    printf("%s(", system_call.name);
-    for (int i = 0; i < system_call.nb_arg; i++) {
-        if (get_number_arg(system_call, i) != 0)
-            printf("%#x", get_number_arg(system_call, i));
-        if (i + 1 < system_call.nb_arg)
-            printf(", ");
-    }
-    printf(") = %#x\n", system_call.rax);
-}
-
 void detect_fonction(struct user_regs_struct regs)
 {
     syscall_t system_call = {regs.orig_rax, "?", 6, regs.rax, regs.rdi,
@@ -58,6 +29,7 @@ void detect_fonction(struct user_regs_struct regs)
             system_call.nb_arg = table[i].nb_arg;
         }
     }
+    display_info(system_call);
 }
 
 void handle_core_pid(char **av, bool s)
@@ -80,9 +52,4 @@ void handle_core_pid(char **av, bool s)
             detect_fonction(regs);
         }
     }
-}
-
-void handle_core_command(char **av, bool s)
-{
-
 }
