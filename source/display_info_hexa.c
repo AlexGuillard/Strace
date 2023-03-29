703d81f32a2c4c284d73cb0422864f158eb6025c
Matthias Gst <matthias.gaste@epitech.eu>
2023-03-29 19:55:52 +0200

    [fix] - display_info_hexa.c - replace get_nb_ar function in the file

--- source/display_info_hexa.c
+++ source/display_info_hexa.c
@@ -7,6 +7,26 @@
 
 #include "strace.h"
 
+static int get_number_arg(syscall_t *system_call, int i)
+{
+    switch (i) {
+    case 0:
+        return system_call->rdi;
+    case 1:
+        return system_call->rsi;
+    case 2:
+        return system_call->rdx;
+    case 3:
+        return system_call->rcx;
+    case 4:
+        return system_call->r8;
+    case 5:
+        return system_call->r9;
+    default:
+        return 0;
+    }
+}
+
 void display_info_hexa(syscall_t *system_call)
 {
     if (strcmp(system_call->name, "?") == 0)
